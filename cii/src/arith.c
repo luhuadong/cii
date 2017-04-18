b7a162e37c0a1b17523f89973929b54d6552709e
rudy <luhuadong@163.com>
2017-04-18 23:41:57 +0800

    add stack.h

--- cii/src/arith.c
+++ cii/src/arith.c
@@ -1,15 +1,27 @@
 #include <arith.h>
 
+/* 
+ * Return max between x and y
+ *
+ */
 int Arith_max(int x, int y)
 {
 	return x > y ? x : y;
 }
 
+/* 
+ * Return min between x and y
+ *
+ */
 int Arith_min(int x, int y)
 {
 	return x > y ? y : x;
 }
 
+/* 
+ * Return max between x and y
+ *
+ */
 int Arith_div(int x, int y) {
 	if (-13/5 == -2
 	&&	(x < 0) != (y < 0) && x%y != 0)
@@ -17,6 +29,11 @@ int Arith_div(int x, int y) {
 	else
 		return x/y;
 }
+
+/* 
+ * Return max between x and y
+ *
+ */
 int Arith_mod(int x, int y) {
 	if (-13/5 == -2
 	&&	(x < 0) != (y < 0) && x%y != 0)
@@ -25,11 +42,19 @@ int Arith_mod(int x, int y) {
 		return x%y;
 }
 
+/* 
+ * Return max between x and y
+ *
+ */
 int Arith_floor(int x, int y)
 {
 	return Arith_div(x, y);
 }
 
+/* 
+ * Return max between x and y
+ *
+ */
 int Arith_ceiling(int x, int y)
 {
 	return Arith_div(x, y) + (x%y != 0);
